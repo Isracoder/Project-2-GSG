655c6bd41365b1f090a1e3d13a4bd4d25e9c4b4b
Isracoder <90979049+Isracoder@users.noreply.github.com>
2023-05-20 12:10:18 +0300

    Update project2.js
    an update with working user input

--- project2.js
+++ project2.js
@@ -1,41 +1,36 @@
-const readline = require('readline') ;
-let r1 = readline.createInterface({
-    input: process.stdin , 
-    output: process.stdout
-}
-) ;
-//const prompt = require('prompt-sync')() ;
+
+const prompt = require("prompt-sync")({ sigint: true });
+
 class Task {
-    // constructor(  priority , dueDate , description){
-    //     this.priority = Number(priority) ;
-    //     this.dueDate = dueDate ;
-    //     this.description = description ;
-    //     this.complete = false ;
-    // }
+    
     printTask =  function(){
+        console.log("-----------------");
+        console.log(`The task has an id of : ${this.id}`) ;
         console.log(`The task description is : ${this.description}`) ;
         console.log(`It has a priority of ${this.priority}`) ;
         console.log(`The due date is ${this.dueDate}`) ;
         console.log(`The task is ${this.complete ? "" : "not "}completed`);
+        console.log("-----------------");
     }
 }
+// default values
+let id = 1 ;
 Task.prototype.dueDate = "4/5/2023" ;
 Task.prototype.priority = 4 ;// low priority 1 , high priority 5 , 1-5
 Task.prototype.desc = "Finishing the project"
-
+Task.prototype.id = id ;
 let tasks = [] ;
+let num = -1 ;
 const main = function(){
-    let num = -1 ;
+    num = -1 ;
     num = printOptions(num) ;
     
-    // while (num < 1 || num > 8){
-    //     console.log("*") ;
-    //     num = printOptions(num) ;
-    //     process.stdin.pause(); 
-    //     if (num < 1 || num > 8)console.log("That was not an option , try again!") ;
-    // }
-    console.log(`num ${num}`) ;
-    console.log("hi") ;
+    while (num < 1 || num > 9 || isNaN(num)){
+        console.log("That was not an option , try again!") ;
+        num = printOptions(num) ;
+         
+    }
+    console.log() ;
     switch(num){
         case 1 : 
             createNewTask() ;
@@ -61,13 +56,21 @@ const main = function(){
         case 8 : 
             clearAll() ;
         break; 
+        case 9 : 
+            console.log("Thank you for using the program") ;
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~") ;
+            console.log() ;
+            break ;
         default : 
         console.log("not an option") ;
         break;
     }
 }
 
-main() ;
+while (num !== 9) main() ; // keep running until the user chooses to exit
+
+
+
 function sortTasksDue(){
     tasks.sort((a , b)=>{
         let firstDate = a.dueDate ;
@@ -77,7 +80,7 @@ function sortTasksDue(){
         return 0;
     })
 }
-function sortByPriority(){
+function sortByPriority(){ // sorting from 5 -> 1
     tasks.sort((a , b)=>{
         return a.priority- b.priority ;
     })
@@ -85,52 +88,62 @@ function sortByPriority(){
 }
 function clearAll(){
     tasks = [] ;
+    console.log("All your tasks have been deleted") ;
+    console.log() ;
 }
 function createNewTask(){
-    console.log("Enter the task priority") ;
-    let priority = 1 ;
-    console.log("Enter the task description") ;
-    let des = "Finishing the project" ;
-    console.log("Enter the due date in dd/mm/y format : ") ;
-    let date = "5/6/2023" ;
-    //creating a task with defualt values until user input problem is fixed
+    //console.log("Enter the task priority") ;
+    let priority = prompt("Enter the task priority as a number from 1-5 : ") ;
+    let des = prompt("Enter the task description : ") ;
+    //let des = "Finishing the project" ;
+    let date = prompt("Enter the due date in dd/mm/y format : ") ;
+    //let date = "5/6/2023" ;
+
     let temporary = new Task ;
     temporary.description = des ;
     temporary.dueDate = date ;
     temporary.priority = priority ;
+    temporary.id = id ;
+    id++ ;
+    console.log("The new task you created : ") ;
+    temporary.printTask() ;
+    console.log() ;
     tasks.push(temporary) ;
 }
 function listTasks(){
     if (tasks.length == 0)console.log("There are no current tasks in your list") ;
     else {
-        for (let i = 0; i < tasks.length; i++){
-            tasks[i].printTask() ;
-        }
+        console.log("This is the current task list : ") ;
+        tasks.forEach(task => task.printTask()) ;
     }
 }
 function listCompletedTasks(){
-    let areComp = false ;
-    for (let i = 0; i < tasks.length; i++){
-        if (tasks[i].complete){
-            areComp = true ;
-            tasks[i].printTask() ;
-        }
+    let complete = tasks.filter(task => task.complete == true) ;
+    if (complete.length == 0)console.log("There are no currently completed tasks") ;
+    else {
+        console.log("These are the completed tasks") ;
+        complete.forEach(task => task.printTask()) ;
     }
-    if (!areComp)console.log("There are no currently completed tasks") ;
 }
 function markAsDone(){
     listTasks(); 
-    console.log("Enter the description of the task you want to mark complete") ;
-    let desc = "userInput".toLowerCase() ;
+    
+    let number = prompt("Enter the id of the task you want to mark complete : ");
     tasks.filter(task => {
-        if(task.description.toLowerCase() == desc) return task })
+        if(task.id == number) return task })
         .forEach(task => task.complete = true )
 }
 function deleteTask(){
     listTasks() ;
-    console.log("Enter the number of the task you would like to delete") ;
-    let num = 1 ;
-    if (num < tasks.length)tasks.splice(num-1 , 1) ;
+    if (tasks.length == 0)return ;
+    let num = prompt("Enter the id of the task you would like to delete : ") ;
+    let ind = 0 ;
+    tasks.forEach((task , index) => {
+        if (task.id == num)ind = index ;
+    })
+    tasks.splice(ind , 1) ;
+    console.log("The new task list ") ;
+    listTasks() ;
 }
 function printOptions(num){
     console.log() ;
@@ -146,51 +159,15 @@ function printOptions(num){
     console.log("6) Sort tasks by the due date")
     console.log("7) Sort tasks by priority")
     console.log("8) Clear all tasks")
+    console.log("9) Exit the program") ;
     console.log("********************") ;
-    //console.log("What's your choice ? ") ;
-    let a = 1 ;
-    // process.stdin.once("line" , function(data){
-    //     a = Number(data) ;
-    //     return a ;
-        
-    //     //process.exit() ;
-    // }) ;
-    // process.stdin.on('end' , function(){
-    //     console.log("he") ;
-    //     return a ;
-    // })
-    // process.on('exit' , function(){
-    //     console.log("hiii") ;
-    //     return Number(a) ;
-    // })
-    // const ac = new AbortController();
-    // const signal = ac.signal;
-
-    // r1.question("What is your choice ? " , (choice)=>{
-    //         a = Number(choice) ;
-    //         r1.close() ;
-    //     })
-    // signal.addEventListener('abort', () => {
-    // console.log('The question timed out');
-    // }, { once: true });
-
-    // setTimeout(() => ac.abort(), 5000);
-
-    console.log(`a ${a}`) ;
-    // let a = prompt("What's your choice ? " , )
-    // prompt.();
-    // console.log(a) ;
-    return Number(a) ;
     
     
     
-    // process.stdin.on('-1' , data => {
-    //     num = Number(data) ;
-    //     console.log(num) ;
-    //     console.log(data) ;
-    //     console.log(process.stdin.read)
-    //     return Number(num)
-        
-    // })
+    num = prompt("What's your choice ? ")    
+
+    
+    return Number(num) ;
+    
      
 }
